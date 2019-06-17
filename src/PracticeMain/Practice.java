1ba3b9651392945e7102d915e8cc2613c4d15135
tnals634 <tnals634@naver.com>
2019-06-17 11:57:09 +0900

    practice_1 end

--- src/PracticeMain/Practice.java
+++ src/PracticeMain/Practice.java
@@ -1,10 +1,10 @@
 package PracticeMain;
-
+import Practices.Practice_1;
 public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Practice_1.practice1();
 	}
 
 }
--- src/Practices/Practice_1.java
+++ src/Practices/Practice_1.java
@@ -1,5 +1,10 @@
 package Practices;
 
 public class Practice_1 {
-
+	public static void practice1() {
+		double c = 31.5, b = 0;
+		b = (c*1.8)+32;
+		System.out.println("¼·¾¾ : "+c);
+		System.out.println("È­¾¾ : "+b);
+	}
 }
