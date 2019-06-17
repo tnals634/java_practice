10a0e4997989324096633bc908d67c61a86991ee
tnals634 <tnals634@naver.com>
2019-06-17 12:16:28 +0900

    practice_3 end

--- src/PracticeMain/Practice.java
+++ src/PracticeMain/Practice.java
@@ -1,12 +1,14 @@
 package PracticeMain;
 import Practices.Practice_1;
 import Practices.Practice_2;
+import Practices.Practice_3;
 public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Practice_1.practice1();
-		Practice_2.practice2();
+		//Practice_1.practice1();
+		//Practice_2.practice2();
+		Practice_3.practice3();
 	}
 
 }
---
+++ src/Practices/Practice_3.java
@@ -0,0 +1,14 @@
+package Practices;
+
+public class Practice_3 {
+	public static void practice3() {
+		for(int i=2;i<10;i++)
+		{
+			System.out.println("** "+i+" ´Ü **");
+			for(int j=1;j<10;j++)
+			{
+				System.out.println(i+" x "+j+" = "+i*j);
+			}
+		}
+	}
+}
