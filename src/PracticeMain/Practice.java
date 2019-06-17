cb24aa96816955c42b57f5dd195f4bbad72dcf47
tnals634 <tnals634@naver.com>
2019-06-17 12:07:12 +0900

    practice_2 end

--- src/PracticeMain/Practice.java
+++ src/PracticeMain/Practice.java
@@ -1,10 +1,12 @@
 package PracticeMain;
 import Practices.Practice_1;
+import Practices.Practice_2;
 public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Practice_1.practice1();
+		Practice_2.practice2();
 	}
 
 }
---
+++ src/Practices/Practice_2.java
@@ -0,0 +1,12 @@
+package Practices;
+
+public class Practice_2 {
+	public static void practice2() {
+		int a = 0;
+		for(int i=1;i<101;i++)
+		{
+			a += i;
+		}
+		System.out.println("гу╟Х : "+a);
+	}
+}
